00f221d26bf00f86af3726c005fc40a500005188
Hooder <hoodernew@gmail.com>
2024-09-06 16:01:12 +1200

    V0.4
    Debugging is in need of order

--- VitualWalkthrough/Main.cpp
+++ VitualWalkthrough/Main.cpp
@@ -5,5 +5,20 @@
 
 int main() 
 {
+	glfwInit(); // creates window
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // GL needs to know which version it's using
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // GL needs to know which profile to use
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL); // creates pop up window
+	if (window == NULL) // fail safe net
+	{
+		std::cout << "Failed to create GL Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwTerminate(); // closes window
 	return 0;
 }
