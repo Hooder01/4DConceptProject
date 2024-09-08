9882ebe59d35a77f54671f9978cc2b1493b9a26e
Hooder <hoodernew@gmail.com>
2024-09-08 23:53:42 +1200

    V0.5
    Swear I already did a 0.5
    
    Window application now spawns like expected and basic color rendering is active.
    
    a bunch of comments for future uses..

--- VitualWalkthrough/Main.cpp
+++ VitualWalkthrough/Main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include<iostream> // This includes the Input/Output Stream library
+#include<glad/glad.h> // This includes the GLAD (OpenGL Loader) library
+#include<GLFW/glfw3.h> // This includes the GLFW library, which is used for creating windows
 
 
 int main() 
@@ -11,14 +11,33 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // GL needs to know which profile to use
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL); // creates pop up window
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Vitual4DSpace", NULL, NULL); // creates the pop up window
 	if (window == NULL) // fail safe net
 	{
 		std::cout << "Failed to create GL Window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	glfwMakeContextCurrent(window);
 
-	glfwTerminate(); // closes window
+
+	gladLoadGL(); // This loads all the OpenGL function pointers using the GLAD loader
+
+	glViewport(0, 0, 800, 800); // Area of which part of the window will render! (this should be the same as "createwindow")
+
+	glClearColor(0.224f, 0.224f, 0.224f, 0.0f); // Color RGB values
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window); // This swaps the front and back buffers of the specified window
+
+
+	while (!glfwWindowShouldClose(window)) // This loop continues running as long as the window should remain open
+	{
+		glfwPollEvents(); // This processes all pending events, It ensures the application stays responsive to user input
+	}
+
+
+
+	glfwDestroyWindow(window); // This destroys the specified window and its context, freeing up the resources used by the window.
+	glfwTerminate(); // closes/terminates window
 	return 0;
 }
